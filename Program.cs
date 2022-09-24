ceab2d6fe2c0bec06ca333a0e14a27b0706200dd
Aleksei Kulberg <aleksei.kulberg.work@gmail.com>
2022-09-24 18:14:16 +0300

    Add task one implementation

--- Program.cs
+++ Program.cs
@@ -22,20 +22,58 @@ namespace Pr7
             return randomArray;
         }
 
+        //Выполнение задания №1
+        #region TASK1
 
-
-        static void Main(string[] args)
+        static Dictionary<int, double> GetSumIfContainsNegative(double[][] array)
         {
-            double[][] array_test = GenRanomArray(10, 10);
-
-            for (int i = 0; i < array_test.Length; ++i)
+            Func<double[], double> getArraySum = str =>
             {
-                for (int j = 0; j < array_test[i].Length; ++j)
+                double sum = 0;
+                for (int i = 0; i < str.Length; ++i)
+                    sum += str[i];
+                return sum;
+            };
+
+            Dictionary<int, double> result = new Dictionary<int, double>();
+
+            for (int i = 0; i < array.Length; ++i)
+            {
+                if (array[i].Any(n => n < 0))
+                    result.Add(i, getArraySum(array[i]));
+            }
+            return result;
+        }
+
+        //w = array width, h = array height
+        static void Task1Check(int w, int h)
+        {
+            double[][] array_1 = GenRanomArray(w, h);
+            Dictionary<int, double> array1_sums = GetSumIfContainsNegative(array_1);
+
+           
+            Console.WriteLine("****[TASK1]****\n");
+            Console.WriteLine("Plain array:");
+            for (int i = 0; i < array_1.Length; ++i)
+            {
+                for (int j = 0; j < array_1[i].Length; ++j)
                 {
-                    Console.Write($"{array_test[i][j]}\t");
+                    Console.Write($"{array_1[i][j]}\t");
                 }
                 Console.WriteLine("");
             }
+            Console.WriteLine("\nResult:");
+            for (int i = 0; i < array1_sums.Count; ++i)
+            {
+                var LineSum = array1_sums.ToList()[i];
+                Console.WriteLine($"Line: \"{LineSum.Key}\"\t Sum: \"{LineSum.Value}\"");
+            }
+        }
+        #endregion //TASK1
+
+        static void Main(string[] args)
+        {
+            Task1Check(20, 10);
         }
     }
 }
